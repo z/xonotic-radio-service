7527962371d41fa420dedf5d441a6cbc53947ae4
z <z@xnz.me>
2016-02-15 23:56:11 -0500

    inital commit with basics covered

---
+++ README.md
@@ -0,0 +1,50 @@
+## Setup
+
+#### System Requirements
+
+```
+ffmpeg
+```
+
+Here's a PPA if you need it
+
+```
+sudo add-apt-repository ppa:kirillshkrogalev/ffmpeg-next
+sudo apt-get update
+sudo apt-get install ffmpeg
+```
+
+#### Setup local packages
+
+Clone from git
+
+```
+git clone ...
+```
+
+Setup a venv and install the requirements
+
+```
+virtualenv -p /usr/bin/python3 venv
+ln -s venv/bin/activate
+source activate
+pip install -r requirements.txt
+```
+
+## Usage
+
+Make sure youtube2pk3.py is chmod +x, `chmod +x youtube2pk3.py`, then run it as follows:
+
+
+```
+./youtube2pk3.py https://www.youtube.com/watch\?v\=dz24DgBUQbc
+```
+
+endpoint.txt output:
+
+
+```
+http://example.com/radio/packages/dz24DgBUQbc.pk3 276 Vanic X K.Flay - Make Me Fade%
+```
+
+Your packages folder should contain a pk3 with an ogg file inside of it that matches the endpoint.txt
---
+++ youtube2pk3.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# z@xnz.me
+import argparse, zipfile, subprocess, os, pafy
+
+
+def main():
+
+    # Config
+    site_url = 'http://example.com/radio/'
+    endpoint_file = 'endpoint.txt'
+    package_path = 'packages/'
+    cache_path = 'cache/'
+
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("youtube_url", help="A youtube video URL",
+                        type=str)
+    args = parser.parse_args()
+
+    # Setup vars
+    youtube_url = args.youtube_url
+    video = pafy.new(youtube_url)
+    formatted_duration = video.duration
+
+    ftr = [3600,60,1]
+
+    duration = sum([a*b for a,b in zip(ftr, map(int,formatted_duration.split(':')))])
+
+    print("processing: " + youtube_url)
+    print("title: " + video.title)
+    print("duration: " + formatted_duration)
+
+    print("Available audio streams:")
+    audiostreams = video.audiostreams
+    for a in audiostreams:
+        print(a.bitrate, a.extension, a.get_filesize())
+
+    bestaudio = video.getbestaudio()
+    
+    music_file = bestaudio.download(cache_path)
+
+    ogg_file = video.videoid + '.ogg'
+    pk3_file = video.videoid + '.pk3'
+
+    # Convert whatever (hopefully m4a) to ogg -- this part is the most likely to break
+    with open(music_file, 'r') as f:
+        subprocess.call(["ffmpeg", '-i', music_file, '-acodec', 'vorbis', '-strict', '-2', '-aq', '60', '-vn', '-ac', '2', cache_path + ogg_file])
+
+    # Create a zip with the ogg inside of it
+    with zipfile.ZipFile(package_path + pk3_file, 'w') as myzip:
+        myzip.write(ogg_file, os.path.basename(ogg_file))
+        myzip.close()
+
+    # Write the meta info to the endpoint
+    with open(endpoint_file, 'w') as f:
+        f.write(site_url + package_path + pk3_file + " " + str(duration) + " " + video.title)
+        f.close()
+
+if __name__ == "__main__":
+    main()
+
