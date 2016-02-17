88ff182c4292387d9be94b36d269f319197d9323
z <z@xnz.me>
2016-02-16 19:33:22 -0500

    little bit of cleanup and broke out config to separate file. more verbose output.

--- README.md
+++ README.md
@@ -30,13 +30,38 @@ source activate
 pip install -r requirements.txt
 ```
 
+## Configuration
+
+Copy `example.config.ini` and edit `config.ini` to your server settings:
+
+`cp example.config.ini config.ini`
+
+The file should contain something similar to the following:
+
+```
+[default]
+# URL for where the radio pk3s live
+site_url = http://example.com/radio/
+
+# absolute or relative path to an endpoint file
+endpoint_file = /home/radio/web/html/endpoint.txt
+
+# absolute or relative path to where the packages live
+package_path = /home/radio/web/html/radio/
+
+# absolute or relative path for cached media files
+cache_path = cache/  
+```
+
+Paths can be either absolute or relative. It would be good practice to have this live in a non-web accessible, but allow it to write the packages to a web facing folder.
+
 ## Usage
 
 Make sure youtube2pk3.py is chmod +x, `chmod +x youtube2pk3.py`, then run it as follows:
 
 
 ```
-./youtube2pk3.py https://www.youtube.com/watch\?v\=dz24DgBUQbc
+./youtube2pk3.py "https://www.youtube.com/watch?v=dz24DgBUQbc"
 ```
 
 endpoint.txt output:
--- youtube2pk3.py
+++ youtube2pk3.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 # z@xnz.me
-import argparse, zipfile, subprocess, os, pafy
+import argparse, configparser, zipfile, subprocess, os, pafy
 
 
 def main():
 
     # Config
-    site_url = 'http://example.com/radio/'
-    endpoint_file = 'endpoint.txt'
-    package_path = 'packages/'
-    cache_path = 'cache/'
+    if not os.path.isfile('config.ini'):
+        print('config.ini not found, please create one.')
+        return 1
+
+    config = configparser.ConfigParser()
+
+    config.read('config.ini')
+
+    site_url = config['default']['site_url']
+    endpoint_file = config['default']['endpoint_file']
+    package_path = config['default']['package_path']
+    cache_path = config['default']['cache_path']
 
     # Parse args
     parser = argparse.ArgumentParser()
@@ -39,23 +47,28 @@ def main():
     
     music_file = bestaudio.download(cache_path)
 
-    ogg_file = video.videoid + '.ogg'
+    ogg_file = cache_path + video.videoid + '.ogg'
     pk3_file = 'yt-' + video.videoid + '.pk3'
 
     # Convert whatever (hopefully m4a) to ogg -- this part is the most likely to break
     with open(music_file, 'r') as f:
+        print('converting to ogg...')
         #subprocess.call(["ffmpeg", '-i', music_file, '-acodec', 'vorbis', '-strict', '-2', '-aq', '60', '-vn', '-ac', '2', cache_path + ogg_file])
         subprocess.call(['avconv', '-i', music_file, '-codec:a', 'libvorbis', '-qscale:a', '5', ogg_file])
 
     # Create a zip with the ogg inside of it
     with zipfile.ZipFile(package_path + pk3_file, 'w') as myzip:
+        print('creating zip...')
         myzip.write(ogg_file, os.path.basename(ogg_file))
         myzip.close()
 
     # Write the meta info to the endpoint
     with open(endpoint_file, 'w') as f:
+        print('writing endpoint file...')
         f.write(site_url + pk3_file + " " + str(duration) + " " + video.title)
         f.close()
+
+    print('done.')
 
 if __name__ == "__main__":
     main()
