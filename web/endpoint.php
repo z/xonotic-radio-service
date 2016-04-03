b809f87dcee7996335dcd403fcb18123db9610c0
z <z@xnz.me>
2016-04-03 13:00:01 -0400

    updated endpoint code

--- web/endpoint.php
+++ web/endpoint.php
@@ -1,8 +1,8 @@
 <?php
 $target = $_GET['t'];
-$config = parse_ini_file("sample.ini");
+$config = parse_ini_file("config/config.ini", true);
 $endpoints = $config['endpoints'];
-if ( array_key_exists($endpoints[$target] ) {
+if ( array_key_exists($target, $endpoints) ) {
     $endpoint_file = $endpoints[$target];
 } else {
     $endpoint_file = $endpoints['default'];
@@ -12,3 +12,4 @@ $contents = file($endpoint_file);
 $line = $contents[rand(0, count($contents) - 1)];
 echo $line;
 ?>
+
