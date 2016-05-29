919a337c47cb32d578f90926911404e3715f1ca8
z <z@xnz.me>
2016-05-29 13:41:16 -0400

    refactored to create a Track object

--- audio2pk3.py
+++ audio2pk3.py
@@ -15,10 +15,6 @@ import mutagen
 from shutil import copyfile
 
 
-conf = {}
-endpoints = {}
-
-
 def main():
 
     global conf
@@ -38,16 +34,7 @@ def main():
     input_source = args.input_source
 
     if re.match('^http(s)?://(www\.|m\.)?(youtube\.com|youtu\.be)/.*', input_source):
-        yt_info = get_audio_from_youtube(input_source)
-
-        output_music_file = yt_info['music_file']
-        duration = yt_info['duration']
-        video = yt_info['video']
-        name = video.videoid
-        if args.title:
-            title = args.title
-        else:
-            title = video.title
+        track = Track(youtube_url=input_source)
     else:
 
         if not args.title:
@@ -56,106 +43,174 @@ def main():
         else:
             title = args.title
 
-        extension = str(os.path.splitext(input_source)[1])
-        name = str(uuid.uuid1())
-        local_name = name + "_d"
-        output_music_file = conf['cache_path'] + local_name + extension
-
         if re.match('^http(s)?://.*', input_source):
-            urllib.request.urlretrieve(input_source, output_music_file, reporthook)
+            track = Track(source_url=input_source, title=title)
         else:
-            copyfile(input_source, output_music_file)
+            track = Track(source_file=input_source, title=title)
 
-        element = mutagen.File(output_music_file)
+    name = str(uuid.uuid1())
 
-        if element:
-            duration = "{0:.2f}".format(element.info.length)
-        else:
-            print('Invalid audio file.')
-            raise SystemExit
-
-    ogg_file = convert_to_ogg(output_music_file, name)
-
-    pk3_file = create_pk3(ogg_file, name)
-
-    write_to_endpoint_list(target, pk3_file, ogg_file, duration, title)
+    track.convert_to_ogg(name)
+    track.create_pk3(name)
+    track.write_to_endpoint_list(target)
 
     print('done.')
 
 
-def get_audio_from_youtube(youtube_url):
+class Track(object):
+    """A rainbow object that can be applied to a string"""
 
-    global conf
+    def __init__(self, pk3_file=None, youtube_url=None, source_url=None, source_file=None, title=None):
 
-    video = pafy.new(youtube_url)
-    formatted_duration = video.duration
+        if not youtube_url and title is None:
+            raise RuntimeError('title required for url or local files')
 
-    ftr = [3600, 60, 1]
+        self.local_file = None
+        self.duration = None
+        self.ogg_file = None
+        self.pk3_file = None
+        self.youtube_url = youtube_url
+        self.source_url = source_url
+        self.source_file = source_file
 
-    duration = sum([a*b for a,b in zip(ftr, map(int,formatted_duration.split(':')))])
-
-    print("processing: " + youtube_url)
-    print("title: " + video.title)
-    print("duration: " + formatted_duration)
-
-    print("Available audio streams:")
-
-    audiostreams = video.audiostreams
-
-    for a in audiostreams:
-        print(a.bitrate, a.extension, a.get_filesize())
-
-    bestaudio = video.getbestaudio()
-    
-    music_file = bestaudio.download(conf['cache_path'])
-
-    return {'music_file': music_file, 'video': video, 'duration': duration}
-
-
-def convert_to_ogg(music_file, name):
-
-    global conf
-
-    ogg_file = name + '.ogg'
-
-    # Convert whatever (hopefully m4a) to ogg -- this part is the most likely to break
-    with open(music_file, 'r') as f:
-        print('converting to ogg...')
-
-        if conf['encoding_driver'] == 'avconv':
-            subprocess.call(['avconv', '-i', music_file, '-codec:a', 'libvorbis', '-b:a', conf['bitrate'], '-vn', conf['cache_path'] + ogg_file])
-        elif conf['encoding_driver'] == 'ffmpeg':
-            subprocess.call(["ffmpeg", '-i', music_file, '-codec:a', 'libvorbis', '-b:a', conf['bitrate'], '-vn', conf['cache_path'] + ogg_file])
+        if youtube_url:
+            self.get_audio_from_youtube(youtube_url)
+        elif source_url:
+            self.download_track(source_url)
+        elif source_file:
+            self.copy_track(source_file)
         else:
-            print('/!\ No valid driver was chosen, please configure either avconv or ffmpeg. Exiting...')
-            raise SystemExit
+            raise RuntimeError('youtube_url, source_url or source_file is required!')
 
-    return ogg_file
+        if title:
+            self.title = title
 
+    def get_audio_from_youtube(self, youtube_url):
 
-def create_pk3(ogg_file, name):
+        if not re.match('^http(s)?://(www\.|m\.)?(youtube\.com|youtu\.be)/.*', youtube_url):
+            raise RuntimeError('URL for youtube did not match pattern.')
 
-    global conf
+        video = pafy.new(youtube_url)
+        formatted_duration = video.duration
 
-    pk3_file = conf['package_prefix'] + name + '.pk3'
+        ftr = [3600, 60, 1]
 
-    # Create a zip with the ogg inside of it
-    with zipfile.ZipFile(conf['package_path'] + pk3_file, 'w') as pk3:
-        print('creating zip...')
-        pk3.write(conf['cache_path'] + ogg_file, os.path.basename(ogg_file))
-        pk3.close()
+        duration = sum([a * b for a, b in zip(ftr, map(int, formatted_duration.split(':')))])
 
-        return pk3_file
+        print("processing: " + youtube_url)
+        print("title: " + video.title)
+        print("duration: " + formatted_duration)
 
+        print("Available audio streams:")
 
-def write_to_endpoint_list(target, pk3_file, ogg_file, duration, title):
+        audio_streams = video.audiostreams
 
-    global conf
+        for a in audio_streams:
+            print(a.bitrate, a.extension, a.get_filesize())
 
-    with open(target, 'a') as f:
-        print('writing to endpoint list file...')
-        f.write(conf['site_url'] + pk3_file + ' ' + ogg_file + ' ' + str(duration) + ' ' + title + '\n')
-        f.close()
+        best_audio = video.getbestaudio()
+
+        music_file = best_audio.download(conf['cache_path'])
+
+        audio_info = {'local_file': music_file, 'video': video, 'duration': duration, 'title': video.title}
+
+        self.youtube_url = youtube_url
+        self.local_file = audio_info['local_file']
+        self.duration = audio_info['duration']
+        self.title = audio_info['title']
+
+        return audio_info
+
+    def download_track(self, source_url):
+
+        extension = str(os.path.splitext(source_url)[1])
+        name = str(uuid.uuid1())
+        local_name = name + "_d"
+        local_file = conf['cache_path'] + local_name + extension
+
+        urllib.request.urlretrieve(source_url, local_file, reporthook)
+
+        duration = self.get_duration(local_file)
+
+        audio_info = {'duration': duration, 'local_file': local_file}
+
+        self.source_url = source_url
+        self.local_file = audio_info['local_file']
+        self.duration = audio_info['duration']
+
+        return audio_info
+
+    def copy_track(self, source_file):
+
+        extension = str(os.path.splitext(source_file)[1])
+        name = str(uuid.uuid1())
+        local_name = name + "_d"
+        local_file = conf['cache_path'] + local_name + extension
+
+        copyfile(source_file, local_file)
+
+        duration = self.get_duration(source_file)
+
+        audio_info = {'duration': duration, 'local_file': local_file}
+
+        self.local_file = audio_info['local_file']
+        self.duration = audio_info['duration']
+
+        return audio_info
+
+    def convert_to_ogg(self, name):
+
+        ogg_file = name + '.ogg'
+
+        # Convert whatever (hopefully m4a) to ogg -- this part is the most likely to break
+        with open(self.local_file, 'r') as f:
+            print('converting to ogg...')
+
+            if conf['encoding_driver'] == 'avconv' or conf['encoding_driver'] == 'ffmpeg':
+                subprocess.call(
+                    [conf['encoding_driver'], '-i', self.local_file, '-codec:a', 'libvorbis', '-b:a', conf['bitrate'],
+                     '-vn',
+                     conf['cache_path'] + ogg_file])
+            else:
+                print('/!\ No valid driver was chosen, please configure either avconv or ffmpeg. Exiting...')
+                raise SystemExit
+
+        self.ogg_file = ogg_file
+
+        return ogg_file
+
+    def create_pk3(self, name):
+
+        pk3_file = conf['package_prefix'] + name + '.pk3'
+
+        # Create a zip with the ogg inside of it
+        with zipfile.ZipFile(conf['package_path'] + pk3_file, 'w') as pk3:
+            print('creating zip...')
+            pk3.write(conf['cache_path'] + self.ogg_file, os.path.basename(self.ogg_file))
+            pk3.close()
+
+            self.pk3_file = pk3_file
+
+            return pk3_file
+
+    def write_to_endpoint_list(self, target):
+
+        with open(target, 'a') as f:
+            print('writing to endpoint list file...')
+            f.write(conf['site_url'] + self.pk3_file + ' ' + self.ogg_file + ' ' + str(self.duration) + ' ' + self.title + '\n')
+            f.close()
+
+    @staticmethod
+    def get_duration(local_file):
+
+        element = mutagen.File(local_file)
+
+        if element:
+            duration = "{0:.2f}".format(element.info.length)
+        else:
+            raise RuntimeError('Invalid audio file.')
+
+        return duration
 
 
 def reporthook(count, block_size, total_size):
